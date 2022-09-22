7ec5cd15dd87cd6c0ed3161c3296706606a71b0e
utsavanand01 <anandutsav456@gmail.com>
2022-09-22 08:34:38 +0530

    first react project

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -6,9 +6,7 @@ export default function Banner() {
            <Container sx={{paddingTop:"90px",paddingLeft:"50px"}}>
                 <Typography   variant="lead" sx={{color:"white",fontSize:"75px",fontWeight:"300"}}>Skill Hai! To Future Hai!</Typography> <br/><br/>
                 <Typography variant="lead" sx={{color:"white",fontSize:"20px",fontWeight:"300"}}>
-                "CWS is an on-demand marketplace for top Programming engineers, developers, consultants, <br/>architects, programmers, and tutors. Get your projects built by vetted web programming freelancers <br/> or learn from expert mentors with team training & coaching experiences in  <b>Project based training.</b>"
-
-
+                "CWS is an on-demand marketplace for top Programming engineers, developers, consultants, <br/>architects, programmers, and tutors. Get your projects built by vetted web programming freelancers <br/> or learn from expert mentors with team training & coaching experiences in  <b>Project based training.</b>
                 </Typography>
            
            </Container>
